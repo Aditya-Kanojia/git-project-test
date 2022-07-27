396970e82834f7208e18852b83a62ee68b007465
Aditya-Kanojia <aditya05kanojia@gmail.com>
2022-07-27 21:16:03 +0530

    added debounce

---
+++ debounce.js
@@ -0,0 +1,25 @@
+
+// let key=document.querySelector('input');
+
+// key.addEventListener('onkeypress',getData)
+let counter=0;
+function getData(){
+
+  console.log("fetching...",counter++);
+    
+}
+
+function debounce(fn,d){
+let timer;
+return function(){
+    let context=this
+    clearInterval(timer);
+    let args=arguments;
+    timer=setTimeout(()=>{
+        fn.apply(context,args)
+    },d)
+}
+}
+
+let callDebounce= debounce(getData,500);
+callDebounce();
